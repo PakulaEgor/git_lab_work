78b586e01cf3b2541f2dc56d6b8fd9fd0741c1f6
Єгор Пакула <pakula.ye@donnu.edu.ua>
2026-06-18 17:17:13 -0400

    feat: додаємо функцію farewell

--- src/main.py
+++ src/main.py
@@ -3,5 +3,8 @@ def greet(name: str) -> str:
     """Повертає вітальне повідомлення."""
     return f"Привіт, {name}!"
 
-if __name__ == "__main__":
+def farewell(name: str) -> str:
+    """Повертає прощальне повідомлення."""
+    return f"До побачення, {name}!"
+ if __name__ == "__main__":
     print(greet("Студент"))
